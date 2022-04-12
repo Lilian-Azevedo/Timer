f110d16f8e2f1b33fc90ca8972c969d4c375d59b
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-04-11 22:00:56 -0300

    chore: song stopped when stop the countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -35,15 +35,18 @@ export default class Countdown extends Component {
     }
   }
 
+  componentWillUnmount() {
+    this.audio.pause();
+  }
+
   togglePlay = () => {
     this.setState({
       play: !this.state.play,
-    });
-    this.state.play ? this.audio.play() : this.audio.pause();
+    }, () => this.state.play ? this.audio.play() : this.audio.pause());
   }
   
   startTimer = () => {
-    this.setState({ stoped: false });
+    this.setState({ stoped: false, play: true });
     this.timer = setInterval(() => {
         this.setState(({seg, min, hour}) => ({
             hour: (seg === 0 && min === 0 && hour > 0 )? hour - 1: hour,
@@ -51,11 +54,13 @@ export default class Countdown extends Component {
             seg: (seg === 0)? 59: seg - 1 ,
         }));
     }, 1000);
+    this.audio.play();
   }
 
   stopTimer = () => {
-    this.setState({ stoped: true });
+    this.setState({ stoped: true, play: false });
     clearInterval(this.timer);
+    this.audio.pause();
   }
 
   render() {
@@ -94,15 +99,18 @@ export default class Countdown extends Component {
             <div>
               <button type='button' onClick={this.togglePlay}>
                 { this.state.play ? 
-                (<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clipRule="evenodd" />
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
-                </svg>)
-                : (
+                (
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
                   </svg>
-                )}
+                )
+                :
+                (
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clipRule="evenodd" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+              </svg>
+              )}
               </button>
             </div>
         </div>
